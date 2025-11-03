7f18e57e42426ce33b415f5197bbd6c64f602785
Cole Canning <ccanning10@gmail.com>
2025-11-02 23:37:22 -0500

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,14 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - api/auth (auth API routes)
+     * - api/webhook (webhook endpoint)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - login (login page)
+     * - saves (public save detail pages)
      */
-    '/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)',
+    '/((?!api/auth|api/webhook|_next/static|_next/image|favicon.ico|login|saves).*)',
   ],
 };
 
